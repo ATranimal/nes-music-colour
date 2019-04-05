dbbcf6a351c30b3e4b0b68023e94a8b178bae7ee
ATranimal <tran.andrew.a@gmail.com>
2019-04-05 16:15:24 -0400

    Initial Commit

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Player from '../components/player/Player';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Player />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/player/Player.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Player extends Component {
+  render() {
+    return (
+      <h1> TEST </h1>
+    );
+  }
+}
+
+export default Player;
